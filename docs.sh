77e06c73c55415a28d295c24750eaba2b161d441
AiwarriorLA <mathew.barrera@linuxacademy.com>
2019-06-12 17:58:53 -0400

    Updating docs

--- docs.sh
+++ docs.sh
@@ -5,6 +5,8 @@ docco lambda/custom/index.js
 git add docs/*
 git stash 
 git checkout gh-pages 
-git stash pop 
+git stash apply 
 git commit -am "Updating docs"
 git push
+git stash drop
+git checkout master
